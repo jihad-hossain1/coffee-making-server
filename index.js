7d946c6979c925ee25560e353da1904d517d3cb0
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-10 07:46:35 +0600

    send data to mongodb database by POST method properly

--- index.js
+++ index.js
@@ -33,10 +33,13 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const coffeeCollection = client.db('coffeeDB').collection('coffee')
 
-        app.post('/coffee', (req, res) => {
+        app.post('/coffee', async (req, res) => {
             const newCoffee = req.body;
             console.log(newCoffee);
+            const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result)
         })
 
 
