8d45bc92fee4397440fd58cd1d7d1c1ebeebf31b
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-10 10:02:36 +0600

    a data delete from database by "delete" method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -48,6 +48,12 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await coffeeCollection.deleteOne(query);
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
